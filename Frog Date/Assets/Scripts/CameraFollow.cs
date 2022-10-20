93fa5d7a1a917bf7372ae95c36b6a77bebbf75b2
Benjamin Hunsicker <benhunsicker1@gmail.com>
2022-10-20 17:23:39 -0400

    Integrate Rozell's work

--- Frog Date/Assets/Scripts/CameraFollow.cs
+++ Frog Date/Assets/Scripts/CameraFollow.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public float multiplier;
+
+
     void Update()
     {
         float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
         float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
-        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 5f), mouseX * 5f, transform.localRotation.z));
+        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * multiplier), mouseX * multiplier, transform.localRotation.z));
     }
 }
--- Frog Date/Assets/Scripts/ClockTime.cs
+++ Frog Date/Assets/Scripts/ClockTime.cs
@@ -15,17 +15,40 @@ public class ClockTime : MonoBehaviour
     private int clockTimeHour = 9;
     private int timeCountMultiplier = 1;
 
-    void Update()
+    private GameObject[] Clickables;
 
+    private void Start()
     {
-       curSec =+ Time.time;
-       Debug.Log(curSec);
-       clockTime = clockTimeHour + ":" + clockTimeMin;
-       flowchart.SetStringVariable("ClockTime", clockTime);
-       if (curSec > 60 * timeCountMultiplier)
-       {
+        Clickables = GameObject.FindGameObjectsWithTag("clickable");
+    }
+
+
+    void Update()
+    {
+        curSec =+ Time.time;
+        //Debug.Log(curSec);
+        clockTime = clockTimeHour + ":" + clockTimeMin;
+        flowchart.SetStringVariable("ClockTime", clockTime);
+        if (curSec > 60 * timeCountMultiplier)
+        {
             clockTimeMin++;
             timeCountMultiplier++;
-       }
+        }
+
+
+        if (flowchart.GetBooleanVariable("Interact") == true)
+        {
+            foreach (GameObject clickable in Clickables)
+            {
+                clickable.GetComponent<Clickable2D>().ClickEnabled = true;
+            }
+        }
+        else
+        {
+            foreach (GameObject clickable in Clickables)
+            {
+                clickable.GetComponent<Clickable2D>().ClickEnabled = false;
+            }
+        }
     }
 }
